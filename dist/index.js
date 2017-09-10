530516f3a94985ea6b0cf97b9fcd5af53fccd2bf
liubinbin <lotosbin@gmail.com>
2017-09-10 13:15:16 +0800

    read/write config file

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
 console.log('hello world!!!');
+var fs = require('fs')
+var jsonfile = require('jsonfile')
+var file = '.xtask'
+if (!fs.existsSync(file)){
+    fs.create
+}
+jsonfile.readFile(file, function (err, obj) {
+    console.dir(obj)
+})
+console.dir(jsonfile.readFileSync(file))
+
+var obj = {tasks: ['JP']}
+
+jsonfile.writeFile(file, obj, {spaces: 2}, function (err) {
+    console.error(err)
+})
